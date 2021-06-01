85638dabf58b0d8119fbfb3b54033512ee390eba
hapisnake <68489853+hapisnake@users.noreply.github.com>
2021-06-01 10:33:11 +0530

    Create facedetection.py

---
+++ facedetection.py
@@ -0,0 +1,42 @@
+# load the library using the import keyword
+# OpenCV must be properly installed for this to work. If not, then the module will not load with an
+# error message.
+
+import cv2
+
+# Gets the name of the image file (filename) from sys.argv
+
+imagePath = #enter the image path here
+cascPath = "haarcascade_frontalface_default.xml"
+
+# This creates the cascade classifcation from file 
+
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# The image is read and converted to grayscale
+
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# The face or faces in an image are detected
+# This section requires the most adjustments to get accuracy on face being detected.
+
+
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(1,1),
+    flags = cv2.CASCADE_SCALE_IMAGE
+)
+
+print("Detected {0} faces!".format(len(faces)))
+
+# This draws a green rectangle around the faces detected
+
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255,0,0), 2)
+
+cv2.imshow("Faces Detected", image)
+cv2.waitKey(0)
